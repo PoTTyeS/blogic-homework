752b75a82f9fc0f66fc6c401454cc3785b45410f
PoTTyeS <herniserver123@gmail.com>
2022-05-04 17:02:28 +0200

    Update api.service.ts

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -10,7 +10,7 @@ import { IApiProps, IApiResponse } from 'src/interfaces/IApi';
 export class ApiService {
 
     /* Variables */
-    baseURL: string = "https://blogic-homework.vercel.app"; 
+    baseURL: string = "https://blogic-hw-be.herokuapp.com"; 
 
     constructor(private http: HttpClient) { }
 
